ffec7d5281d6c4c97c48acf6ef5db3826e91dbfe
SHUBHAM YADAV <93042979+shubhamyadav4665@users.noreply.github.com>
2025-06-29 17:40:23 +0530

    Add files via upload
    Variables, Input/Output, If-Else

---
+++ Age Validator.py
@@ -0,0 +1,5 @@
+age = int(input("Enter age to check eligiblity of vote : "))
+if age >= 18:
+    print("eligible")
+else:
+    print("not eligible")
---
+++ day1.py
@@ -0,0 +1,7 @@
+print("Python-Practice")
+# this is how we add comment by using ctrl + ? or #
+a = 5
+b = 6
+print("a =", a)
+print("b =", b)
+print("a + b =", a + b)
